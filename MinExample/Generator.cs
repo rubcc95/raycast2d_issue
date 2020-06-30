9b2cab7be6009855f6c6739af8c1d90e78fc1aa1
rubcc95 <rubcc95@gmail.com>
2020-06-30 18:33:16 +0200

    Add files via upload

---
+++ MinExample/Generator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public float size;
+    public int width, height;
+    public GameObject item;
+    public static int itemId;
+
+    //it spawns a bunch of "item" next to the other
+    private void Awake()
+    {
+        //items should ignore himself collider. Anyway I've this option set at my project settings, just to remember you because it's might be part of the issue
+        Physics2D.queriesStartInColliders = false;
+
+        SearchAround.size = size;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Instantiate(
+                    item,
+                    new Vector3(size * (((float)(width - 1) / 2f) - i), size * (((float)(height - 1) / 2f) - j)),
+                    Quaternion.identity
+                );
+
+                //this var names the items at their Awake()
+                itemId++;
+            }
+        }
+    }
+}
---
+++ MinExample/SearchAround.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchAround : MonoBehaviour
+{
+    public static float size;
+
+    private static Direction
+        _left = new Direction(Vector2.left, "left"),
+        _right = new Direction(Vector2.right, "right"),
+        _up = new Direction(Vector2.up, "up"),
+        _down = new Direction(Vector2.down, "down");
+
+    private void Awake()
+    {
+        name = Generator.itemId.ToString();
+    }
+
+    private void Start()
+    {
+        //z localScale isn't part of the issue. with localScale = new Vector3(size, size, size) issue keeps there :(
+        transform.localScale = new Vector2(size, size);
+        GetNeighbour(_left);
+        GetNeighbour(_right);
+        GetNeighbour(_up);
+        GetNeighbour(_down);
+    }
+
+    //get the collision at a direction and prints colliders name
+    //ignores himself collision becauseof
+    private void GetNeighbour(Direction dir)
+    {
+        //all layersMask, raycast distance is setted at the size because we should need more to get some item
+        var col = Physics2D.Raycast(transform.position, dir.vector, size).collider;
+        if (col == null) Debug.Log(name + " doesn't collision at " + dir.name);
+        else Debug.Log(name + " collisions with " + col.gameObject.name + " at " + dir.name);
+    }
+
+    private struct Direction
+    {
+        public Direction(Vector2 vector, string name)
+        {
+            this.vector = vector;
+            this.name = name;
+        }
+        public Vector2 vector;
+        public string name;
+    }
+}
